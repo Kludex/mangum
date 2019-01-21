d8985b1ac1d2a2b4e6852ed5bf05df384e02407e
Jordan <jordan@eremieff.com>
2019-01-21 20:19:41 +1100

    Add conftest fixtures for the aws mock event

--- tests/test_aws.py
+++ tests/test_aws.py
@@ -5,61 +5,7 @@ from starlette.responses import PlainTextResponse
 from mangum.handlers.aws import aws_handler
 
 
-def get_mock_event() -> dict:
-    event = {
-        "path": "/test/hello",
-        "body": "123",
-        "headers": {
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-            "Accept-Encoding": "gzip, deflate, lzma, sdch, br",
-            "Accept-Language": "en-US,en;q=0.8",
-            "CloudFront-Forwarded-Proto": "https",
-            "CloudFront-Is-Desktop-Viewer": "true",
-            "CloudFront-Is-Mobile-Viewer": "false",
-            "CloudFront-Is-SmartTV-Viewer": "false",
-            "CloudFront-Is-Tablet-Viewer": "false",
-            "CloudFront-Viewer-Country": "US",
-            "Host": "wt6mne2s9k.execute-api.us-west-2.amazonaws.com",
-            "Upgrade-Insecure-Requests": "1",
-            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.82 Safari/537.36 OPR/39.0.2256.48",
-            "Via": "1.1 fb7cca60f0ecd82ce07790c9c5eef16c.cloudfront.net (CloudFront)",
-            "X-Amz-Cf-Id": "nBsWBOrSHMgnaROZJK1wGCZ9PcRcSpq_oSXZNQwQ10OTZL4cimZo3g==",
-            "X-Forwarded-For": "192.168.100.1, 192.168.1.1",
-            "X-Forwarded-Port": "443",
-            "X-Forwarded-Proto": "https",
-        },
-        "pathParameters": {"proxy": "hello"},
-        "requestContext": {
-            "accountId": "123456789012",
-            "resourceId": "us4z18",
-            "stage": "test",
-            "requestId": "41b45ea3-70b5-11e6-b7bd-69b5aaebc7d9",
-            "identity": {
-                "cognitoIdentityPoolId": "",
-                "accountId": "",
-                "cognitoIdentityId": "",
-                "caller": "",
-                "apiKey": "",
-                "sourceIp": "192.168.100.1",
-                "cognitoAuthenticationType": "",
-                "cognitoAuthenticationProvider": "",
-                "userArn": "",
-                "userAgent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.82 Safari/537.36 OPR/39.0.2256.48",
-                "user": "",
-            },
-            "resourcePath": "/{proxy+}",
-            "httpMethod": "GET",
-            "apiId": "wt6mne2s9k",
-        },
-        "resource": "/{proxy+}",
-        "httpMethod": "GET",
-        "queryStringParameters": {"name": "me"},
-        "stageVariables": {"stageVarName": "stageVarValue"},
-    }
-    return event
-
-
-def test_aws_response() -> None:
+def test_aws_response(mock_data) -> None:
     def app(scope):
         async def asgi(receive, send):
             response = PlainTextResponse("Hello, world!")
@@ -67,7 +13,7 @@ def test_aws_response() -> None:
 
         return asgi
 
-    mock_event = get_mock_event()
+    mock_event = mock_data.get_aws_event()
     response = aws_handler(app, mock_event, {})
 
     assert response == {
@@ -81,7 +27,7 @@ def test_aws_response() -> None:
     }
 
 
-def test_aws_response_with_body() -> None:
+def test_aws_response_with_body(mock_data) -> None:
     def app(scope):
         async def asgi(receive, send):
             request = Request(scope, receive)
@@ -91,7 +37,7 @@ def test_aws_response_with_body() -> None:
 
         return asgi
 
-    mock_event = get_mock_event()
+    mock_event = mock_data.get_aws_event()
     response = aws_handler(app, mock_event, {})
 
     assert response == {
@@ -102,7 +48,7 @@ def test_aws_response_with_body() -> None:
     }
 
 
-def test_aws_binary_response_with_body() -> None:
+def test_aws_binary_response_with_body(mock_data) -> None:
     def app(scope):
         async def asgi(receive, send):
             message = await receive()
@@ -112,7 +58,7 @@ def test_aws_binary_response_with_body() -> None:
 
         return asgi
 
-    mock_event = get_mock_event()
+    mock_event = mock_data.get_aws_event()
     body = b"123"
     body_encoded = base64.b64encode(body)
     mock_event["body"] = body_encoded
@@ -127,9 +73,9 @@ def test_aws_binary_response_with_body() -> None:
     }
 
 
-def test_starlette_aws_response() -> None:
+def test_starlette_aws_response(mock_data) -> None:
 
-    mock_event = get_mock_event()
+    mock_event = mock_data.get_aws_event()
 
     app = Starlette()
 
