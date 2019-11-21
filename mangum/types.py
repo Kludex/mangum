0dbf42da52ee3fb8717431218a18a7e310cec7bf
Steven Kalt <kalt.steven@gmail.com>
2019-11-21 09:43:44 -0500

    fixed code coverage by ignoring pass-statement line

--- mangum/types.py
+++ mangum/types.py
@@ -1,9 +1,9 @@
 import typing
 
-try:  # python 3.8+
-    from typing import Protocol
-except ImportError:  # python 2.7, 3.4-3.7
-    from typing_extensions import Protocol as _Protocol
+try:
+    from typing import Protocol  # python 3.8+
+except ImportError:
+    from typing_extensions import Protocol as _Protocol  # python 2.7, 3.4-3.7
 
     Protocol = typing.cast(typing._SpecialForm, _Protocol)
     # Otherwise, Protocol has incompatible type "typing_extensions._SpecialForm"
@@ -17,4 +17,4 @@ Send = typing.Callable[[Message], typing.Awaitable[None]]
 
 class ASGIApp(Protocol):
     async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
-        ...
+        ...  # pragma: no cover
