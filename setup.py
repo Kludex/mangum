31ca6a25d17ac0689bea04411d5c5dd3b9e89dbf
erm <jordan@eremieff.com>
2020-01-07 22:31:47 +1100

    0.7.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def get_long_description():
 
 setup(
     name="mangum",
-    version="0.6.19",
+    version="0.7.0",
     packages=find_packages(),
     license="MIT",
     url="https://github.com/erm/mangum",
