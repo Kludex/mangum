1554f6c916de9812040427fb713a0df5808760ca
Eduard Iskandarov <eduard.iskandarov@yandex.ru>
2021-02-28 11:52:37 +0500

    ✨ add event source enum with type detection

--- mangum/types.py
+++ mangum/types.py
@@ -1,3 +1,4 @@
+import enum
 import typing
 from typing_extensions import Protocol
 
@@ -10,3 +11,23 @@ Send = typing.Callable[[Message], typing.Awaitable[None]]
 class ASGIApp(Protocol):
     async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
         ...  # pragma: no cover
+
+
+class EventSource(enum.Enum):
+    ALB = enum.auto()
+    ALB_MULTIVALUEHEADERS = enum.auto()
+    API_GW_V1 = enum.auto()
+    API_GW_V2 = enum.auto()
+
+    @classmethod
+    def get_event_source(cls, event: dict) -> "EventSource":
+        version_val = event.get("version", None)
+        multi_value_headers_val = event.get("multiValueHeaders", None)
+        if version_val == "1.0":
+            return cls.API_GW_V1
+        elif version_val == "2.0":
+            return cls.API_GW_V2
+        elif multi_value_headers_val is not None:
+            return cls.ALB_MULTIVALUEHEADERS
+        else:
+            return cls.ALB
