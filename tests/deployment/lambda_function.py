bd595c2be7381f279a03df184fc363a198a824bf
Seth Miller <seth@four43.com>
2021-03-28 15:43:44 -0500

    Fix code quality on demo function

--- tests/deployment/lambda_function.py
+++ tests/deployment/lambda_function.py
@@ -8,21 +8,16 @@ def lambda_handler(event, context):
     # Lambda@Edge is a special kind of special
     if "Records" in event and "cf" in event["Records"][0]:
         return {
-            'status':          200,
-            'headers':         {
-                'content-type': [{
-                    'key':   'content-type',
-                    'value': 'application/json'
-                }]
+            "status": 200,
+            "headers": {
+                "content-type": [{"key": "content-type", "value": "application/json"}]
             },
-            'body':            base64.b64encode(json.dumps(event).encode()).decode(),
-            'bodyEncoding': 'base64',
+            "body": base64.b64encode(json.dumps(event).encode()).decode(),
+            "bodyEncoding": "base64",
         }
 
     return {
-        'statusCode': 200,
-        'headers':    {
-            'content-type': 'application/json'
-        },
-        'body':       json.dumps(event)
+        "statusCode": 200,
+        "headers": {"content-type": "application/json"},
+        "body": json.dumps(event),
     }
