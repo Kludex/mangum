e978887279d8947b5358158ed3232c15485e4d17
Ramazan Elsunakev <35545693+relsunkaev@users.noreply.github.com>
2022-01-29 01:52:12 -0800

    Added explicit python3.9 and 3.10 support (#224)
    * Added python3.9 and 3.10 support;
    - Added python3.9 and 3.10 tests to CI;
    - Added python3.9 and 3.10 classifiers to `setup.py`;
    - Changed `quart` python version constraint;
    - Adjusted tests to reflect the former;
    
    * Fixed `Quart` import conditions in `test_lifespan.py`;
    
    Co-authored-by: Ramazan Elsunkaev <ramazan@up-linq.com>

--- tests/test_lifespan.py
+++ tests/test_lifespan.py
@@ -8,11 +8,10 @@ from starlette.responses import PlainTextResponse
 from mangum import Mangum
 from mangum.exceptions import LifespanFailure
 
-# One (or more) of Quart's dependencies does not support Python 3.8, ignore this case.
-IS_PY38 = sys.version_info[:2] == (3, 8)
+# Quart no longer support python3.6.
 IS_PY36 = sys.version_info[:2] == (3, 6)
 
-if not (IS_PY38 or IS_PY36):
+if not IS_PY36:
     from quart import Quart
 else:
     Quart = None
@@ -269,9 +268,6 @@ def test_starlette_lifespan(mock_aws_api_gateway_event) -> None:
     }
 
 
-@pytest.mark.skipif(
-    IS_PY38, reason="One (or more) of Quart's dependencies does not support Python 3.8."
-)
 @pytest.mark.skipif(IS_PY36, reason="Quart does not support Python 3.6.")
 @pytest.mark.parametrize(
     "mock_aws_api_gateway_event", [["GET", None, None]], indirect=True
