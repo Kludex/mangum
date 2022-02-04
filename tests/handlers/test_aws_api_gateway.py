0d978e87f85f71dd8e6349fceb50d6ca0c728c36
F. St <39712181+Feriixu@users.noreply.github.com>
2022-02-04 14:26:39 +0100

    More reformatting
    Lines were too long

--- tests/handlers/test_aws_api_gateway.py
+++ tests/handlers/test_aws_api_gateway.py
@@ -47,8 +47,8 @@ def get_mock_aws_api_gateway_event(
                 "cognitoAuthenticationType": "",
                 "cognitoAuthenticationProvider": "",
                 "userArn": "",
-                "userAgent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.82 Safari/537.36 OPR/39.0.2256.48",
-                # noqa: E501
+                "userAgent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) "
+                "Chrome/52.0.2743.82 Safari/537.36 OPR/39.0.2256.48",
                 "user": "",
             },
             "resourcePath": "/{proxy+}",
@@ -118,7 +118,8 @@ def test_aws_api_gateway_scope_basic():
             [b"host", b"70ixmpl4fl.execute-api.us-east-2.amazonaws.com"],
             [
                 b"user-agent",
-                b"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 "
+                b"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                b"Chrome/80.0.3987.132 "
                 b"Safari/537.36",
             ],
             [b"x-amzn-trace-id", b"Root=1-5e66d96f-7491f09xmpl79d18acf3d050"],
