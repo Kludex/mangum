1383fcef6d1cbac75eaf8d097fc2b306cb2bdb3f
Jordan Eremieff <jordan@eremieff.com>
2022-02-25 22:02:32 +1100

    :bookmark: 0.13.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def get_long_description():
 
 setup(
     name="mangum",
-    version="0.12.4",
+    version="0.13.0",
     packages=find_packages(exclude=["tests*"]),
     license="MIT",
     url="https://github.com/jordaneremieff/mangum",
