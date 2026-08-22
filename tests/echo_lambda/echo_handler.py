eb4ad08c84833cad0391aa9f6dbf4a38fe6b11fd
Marcelo Trylesinski <marcelotryle@gmail.com>
2026-08-22 11:42:57 +0200

    Resolve merge conflict in echo handler

--- tests/echo_lambda/echo_handler.py
+++ tests/echo_lambda/echo_handler.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-<<<<<<< HEAD
 import json
 
 from mangum import Mangum
@@ -20,13 +19,6 @@ async def app(scope: Scope, receive: Receive, send: Send) -> None:
                 await send({"type": "lifespan.shutdown.complete"})
                 return
 
-=======
-from mangum import Mangum
-from mangum.types import Receive, Scope, Send
-
-
-async def app(scope: Scope, receive: Receive, send: Send) -> None:
->>>>>>> origin/main
     assert scope["type"] == "http"
     body = bytearray()
     while True:
@@ -35,7 +27,6 @@ async def app(scope: Scope, receive: Receive, send: Send) -> None:
         if not message.get("more_body", False):
             break
 
-<<<<<<< HEAD
     response_body = json.dumps(
         {
             "method": scope["method"],
@@ -45,14 +36,6 @@ async def app(scope: Scope, receive: Receive, send: Send) -> None:
             "lifespan_startup_complete": LIFESPAN_STATE["startup_complete"],
         }
     ).encode()
-=======
-    response_body = b'{"method": "%s", "path": "%s", "query": "%s", "body_length": %d}' % (
-        scope["method"].encode(),
-        scope["path"].encode(),
-        scope["query_string"],
-        len(body),
-    )
->>>>>>> origin/main
     await send(
         {
             "type": "http.response.start",
@@ -60,14 +43,7 @@ async def app(scope: Scope, receive: Receive, send: Send) -> None:
             "headers": [[b"content-type", b"application/json"], [b"x-echo", b"mangum"]],
         }
     )
-<<<<<<< HEAD
     await send({"type": "http.response.body", "body": response_body})
 
 
 handler = Mangum(app, lifespan="auto")
-=======
-    await send({"type": "http.response.body", "body": bytes(response_body)})
-
-
-handler = Mangum(app, lifespan="off")
->>>>>>> origin/main
